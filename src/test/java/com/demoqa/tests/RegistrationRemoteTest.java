0651ac8a43ef3af9233a108ebe575d342ca8d100
shqvarluk <shqvarluk@gmail.com>
2023-09-18 22:03:15 +0500

    added_remote_test_for_jenkins

---
+++ src/test/java/com/demoqa/tests/RegistrationRemoteTest.java
@@ -0,0 +1,38 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.RegistrationPage;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.demoqa.utils.TestDateProperties.*;
+
+public class RegistrationRemoteTest extends TestBase {
+
+    @Test
+    @Tag("remote")
+    void successRegistrationFormTest() {
+        RegistrationPage registrationPage = new RegistrationPage();
+
+        registrationPage.openPage()
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setGender(gender)
+                .setUserNumber(phone)
+                .setDateBirth(day, month, year)
+                .setSubject(subject)
+                .setHobbies(hobie)
+                .setPicture(picture)
+                .setAddress(address)
+                .setState(state)
+                .setCty(city)
+                .saveProfile();
+
+        registrationPage.checkResultTableVisible("Thanks for submitting the form")
+                .checkResult(firstName)
+                .checkResult(lastName)
+                .checkResult(email)
+                .checkResult(phone);
+
+    }
+}
